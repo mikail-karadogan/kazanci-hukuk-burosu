45ba9caf732a94529e13b3f93213c742a9be30bf
mikail-karadogan-ntt <mikail.karadogan@bs.nttdata.com>
2024-01-15 16:31:39 +0300

    fix

--- formContact/contact.php
+++ formContact/contact.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 // SMTP ayarları
 $mail = new PHPMailer(true);
